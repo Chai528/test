7fe066d122ccf8674efbd50aeda75780a2f95473
Chai <1367769842@qq.com>
2023-08-10 17:05:43 +0800

    谁帅的while循环

--- test.cpp
+++ test.cpp
@@ -2,7 +2,21 @@
 #include <iostream>
 using namespace std;
 int main() {
-
-
+	cout << "谁是最帅的？" << endl;
+	cout << "1.我" << endl;
+	cout << "2.还是我" << endl;
+	char select;
+	while (1) {
+		cin >> select;
+		if (select == 1){
+			break;
+		}
+		else if (select == 2) {
+			cout << "也算识趣" << endl;
+			system("pause");
+			break;
+		}
+		cout << "你再好好想想？谁说了算" << endl;
+	}
 	return 0;
 }
